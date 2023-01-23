2a08af093fea755dc2b28ac1fda124c81c55586d
Maricris Micael <78831793+prestize@users.noreply.github.com>
2023-01-23 17:43:13 +0800

    recognize upto arithmetic_term

--- syntax_analyzer.c
+++ syntax_analyzer.c
@@ -10,14 +10,18 @@ int position;
 char *lexeme;
 char space[2];
 char *token;
-
+char *expr;
 
 void getNextToken(); // getting the next token in the symbol table
 void stmt(); //check whether the stmt is simple or compound
-int expect(char expectedToken[]); //chek if the next token is the same as the expectedToken
+void expect(char expectedToken[]); //chek if the next token is the same as the expectedToken
 void declaration_stmt(); //check if declaration stmt
 void error(char message[]); //display error message
 bool resv_word(); //check if resv_word
+bool const_wordCharBool(); //check if the toke is a CONSTWORD | CONSTCHARACTER | false | true
+bool arithmetic_expr();
+bool arithmetic_term();
+
 
 
 int main(){
@@ -35,7 +39,12 @@ int main(){
     lexeme = (char*)malloc(sizeof(char)+1);
     lexeme[0] = '\0';
 
-    //token[0] = '\0';
+    token = NULL;
+    token = (char*)malloc(sizeof(char)+1);
+
+    expr = NULL;
+    expr = (char*)malloc(sizeof(char)+1);
+    expr[0] = '\0';
 
     //read file
     stmt();
@@ -47,15 +56,12 @@ int main(){
 
 void getNextToken(){
 
-    token = NULL;
-    token = (char*)malloc(sizeof(char)+1);
+
     token[0] = '\0';
 
     currentChar[0]=fgetc(symbolTable);
     currentChar[1]='\0';
-    
-    
-    //get token
+
     while(currentChar[0]!='~'){
 
         token = (char *)realloc(token, (strlen(token) + strlen(currentChar)) * sizeof(char) + 1);
@@ -70,7 +76,7 @@ void getNextToken(){
     currentChar[1]='\0';
 
 
-    //get lexeme
+
     while(currentChar[0]!='\n'){
 
         lexeme = (char *)realloc(lexeme, (strlen(lexeme) + strlen(currentChar)) * sizeof(char) + 1);
@@ -85,25 +91,36 @@ void getNextToken(){
 
     lexeme = (char *)realloc(lexeme, (strlen(lexeme) + strlen(space)) * sizeof(char) + 1);
     strcat(lexeme,space);
-    //printf("%s\t", token);
+
 
 
 }
 
 
-int expect(char expectedToken[]){
-
-    printf(expectedToken);
-    printf(token);
+void expect(char expectedToken[]){
     getNextToken();
-    if(token==expectedToken){
-        printf(expectedToken);
-        return 1;
+
+    if(strcmp(expectedToken, ";") == 0){
+        if(strcmp(token, expectedToken) == 0){
+            printf("%s ", token);
+            getNextToken();
+        }
+
     }
-    error("1expect: unexpected symbol");
-    return 0;
+    else {
+        if(strcmp(token,expectedToken) == 0){
+            printf("%s ", token);
+        }else{
+        error("expect: unexpected symbol");
+        }
+    }
+
+
 
 }
+
+
+
 
 
 void error(char message[]){
@@ -111,11 +128,10 @@ void error(char message[]){
 }
 
 
-
 void stmt(){
     getNextToken();
-    printf("<stmt>\n");
-    if(token == "if" ||token == "else" ||token == "match" ||token == "while" ||token == "foreach"){
+
+    if(strcmp(token,"if")==0 || strcmp(token,"else")==0 || strcmp(token,"foreach")==0 || strcmp(token,"match") ==0|| strcmp(token,"while")==0){
         ;
         //compound_stmt(lexeme,token);
     }
@@ -125,14 +141,11 @@ void stmt(){
 }
 
 void simple_stmt(){
-    printf("<simple_stmt>\n");
 
     if(resv_word){
-        printf("<declaration_stmt>\n");
-        printf("<resv_word> ");
+        printf("%s ", token);
         declaration_stmt();
-        //optional(;);
-        //expect(NEWLINE);
+        //expect(";");
         printf("\n%s", lexeme);
     }
     else{
@@ -149,21 +162,22 @@ void compound_stmt(){
 void declaration_stmt(){
 
     getNextToken();
-    //printf(token);
 
-    //MAY BUG DITO. AYAW MAG EQUAL NUNG TOKEN AT IDENTIFIER
-    if(token == "IDENTIFIER"){
-        printf(token);
+    if(strcmp(token,"IDENTIFIER")==0){
+        printf("%s ", token);
+        expect("=");
+        getNextToken();
+        const_wordCharBool();
+        arithmetic_expr();
     }else{
-        printf("error");
+        printf("\nerror");
     }
 
 }
 
 bool resv_word(){
 
-
-    if(token == "word" || token == "boolean" || token == "character" || token == "num" || token == "decimal"){
+    if(strcmp(token,"word")==0 || strcmp(token,"character")==0 || strcmp(token,"num")==0 || strcmp(token,"boolean") ==0|| strcmp(token,"decimal")==0){
         return true;
     }
     else{
@@ -173,3 +187,59 @@ bool resv_word(){
 
 
 }
+
+
+bool const_wordCharBool(){
+
+    if(strcmp(token,"CONSTWORD")==0 || strcmp(token,"CONSTCHARACTER")==0){
+        printf("%s ", token);
+        return true;
+    }
+    else if(strcmp(token,"true")==0 || strcmp(token,"false")==0){
+        printf("%s ", token);
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+
+bool arithmetic_expr(){
+
+    //arithmetic_term
+    if(arithmetic_term){
+        printf("%s ", token);
+        return true;
+    }
+    else if(){
+
+    }
+    else{
+        return false;
+    }
+
+}
+
+bool arithmetic_term(){
+
+    //arithmetic_term
+    if(strcmp(token,"IDENTIFIER")==0 || strcmp(token,"CONSTNUM")==0 || strcmp(token,"CONSTDECIMAL")==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+bool highest(){
+
+    if(arithmetic_term){
+
+    }
+
+}
+
+
